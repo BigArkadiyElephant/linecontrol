e3406966973bb7983ce2987c706414ae96df4e8e
BigArkadiyElephant <110096777+BigArkadiyElephant@users.noreply.github.com>
2022-07-29 12:43:54 +0500

    Update ConsoleApplication1.cpp

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <windows.h>
 #include <string>
 
@@ -7,20 +7,19 @@
 using namespace std;
 
 HANDLE connectedPort;
-int selectedPort = 7;
+int selectedPort = 7;           //Необходимо выбрать COM-порт
 bool isConnected = false;
 int targetBaudRate = 57600;
 
-int serialBegin(int BaudRate, int Comport);
-void connectRequest(void);
-void SerialWrite(void);
-char* Buffer = new char[256];
+int serialBegin(int BaudRate, int Comport);     //функция, производящая подключение COM-порта
+void connectRequest(void);                      //функция, которая регулирует подключение и отключение COM-порт
+void SerialWrite(void);                         //функция, передающая данные на COM-порт
 
 int main()
 {
-    connectRequest();
-    SerialWrite();
-    connectRequest();
+    connectRequest();       //подключение
+    SerialWrite();          //работа с COM-портом - отправка данных
+    connectRequest();       //отключение
     return 1;
 }
 
@@ -29,26 +28,26 @@ int serialBegin(int BaudRate, int Comport)
 {
     //CloseHandle(connectedPort);
 
-    connectedPort = CreateFileA(
-        ("\\\\.\\COM" + to_string(Comport)).c_str(),
-        GENERIC_READ | GENERIC_WRITE,
+    connectedPort = CreateFileA(                        //подключение COM-порта
+        ("\\\\.\\COM" + to_string(Comport)).c_str(),    //указание COM-порта
+        GENERIC_READ | GENERIC_WRITE,                   //параметры доступа
         0,
         NULL,
-        OPEN_EXISTING,
+        OPEN_EXISTING,                                  //режим открытия
         FILE_ATTRIBUTE_NORMAL,
         NULL);
 
-    if (connectedPort == INVALID_HANDLE_VALUE)  return -4;  //No port
-    DCB SerialParams;
+    if (connectedPort == INVALID_HANDLE_VALUE)  return -4;  //отсутствие устройств
+    DCB SerialParams;                                       //параметры
     SerialParams.DCBlength = sizeof(SerialParams);
     if (!GetCommState(connectedPort, &SerialParams)) 
-    return -3; //GetState error
+    return -3;                                              //начальные настройки не полученны
 
     SerialParams.BaudRate = BaudRate;
     SerialParams.ByteSize = 8;
     SerialParams.StopBits = ONESTOPBIT;
     SerialParams.Parity = NOPARITY;
-    if (!SetCommState(connectedPort, &SerialParams)) return -2; //SetState error
+    if (!SetCommState(connectedPort, &SerialParams)) return -2; //не удалось задать новые параметры
 
     COMMTIMEOUTS SerialTimeouts;
     SerialTimeouts.ReadIntervalTimeout = 1;
@@ -56,7 +55,7 @@ int serialBegin(int BaudRate, int Comport)
     SerialTimeouts.ReadTotalTimeoutMultiplier = 1;
     SerialTimeouts.WriteTotalTimeoutConstant = 1;
     SerialTimeouts.WriteTotalTimeoutMultiplier = 1;
-    if (!SetCommTimeouts(connectedPort, &SerialTimeouts)) return -1; //SetTimeouts error
+    if (!SetCommTimeouts(connectedPort, &SerialTimeouts)) return -1; //не удалось задать длительность ожидания
 
     return 0;
 }
@@ -72,10 +71,10 @@ void connectRequest(void) {
 
     switch (serialBegin(targetBaudRate, selectedPort))
     {
-    case -4: cout << "error\n";
-    case -3: cout << "error\n";
-    case -2: cout << "error\n";
-    case -1: cout << "error\n";
+    case -4: cout << "No port\n";
+    case -3: cout << "GetState error\n";
+    case -2: cout << "SetState error\n";
+    case -1: cout << "SetTimeouts error\n";
     case 0: 
         cout << "connected\n";
         isConnected = true;
@@ -85,14 +84,7 @@ void connectRequest(void) {
     CloseHandle(connectedPort);
 }
 
-void strСpy(char* str1, char* str2)
-{//               -------->
-    int quant = strlen(str1);
-    for (int i = 0; i <= quant; i++)
-        str2[i] = str1[i];
-}
-
-void addEnld(char* str)
+void addEnld(char* str)         //добавление переноса строки для того, чтобы плата считала данные
 {
     int quant = strlen(str);
     str[quant] = '\n';
@@ -100,7 +92,7 @@ void addEnld(char* str)
 }
 
 void SerialWrite() {
-    if (!isConnected) return;
+    if (!isConnected) return;           //проверка подключения
     DWORD BytesIterated;
 
     do
@@ -108,7 +100,7 @@ void SerialWrite() {
         char* Buffer = new char[256];
         cin.getline(Buffer, 256); 
         addEnld(Buffer);
-        WriteFile(connectedPort, Buffer, strlen(Buffer), &BytesIterated, NULL);
+        WriteFile(connectedPort, Buffer, strlen(Buffer), &BytesIterated, NULL);     //отправка данных на плату
         delete[] Buffer;
 
         cout << "\n--------------\n";
